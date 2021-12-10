133a20a1d9d71e1bf4a723ce7ecebb8944ed5b87
Anton <amrakimmerian@gmail.com>
2021-12-10 12:39:41 +0500

    Pagination. Отображение разбиения на страницы

--- src/app/component/users.jsx
+++ src/app/component/users.jsx
@@ -1,38 +1,54 @@
-import React, { useState } from 'react'
-import api from '../api'
-import User from './user'
-import SearchStatus from './searchStatus'
-import Pagination from './pagination'
+import React, { useState } from "react";
+import api from "../api";
+import User from "./user";
+import SearchStatus from "./searchStatus";
+import Pagination from "./pagination";
 
-const Users = ({users, ...rest}) => {
+const Users = ({ users, ...rest }) => {
+  const columnNames = [
+    "Имя",
+    "Качества",
+    "Профессия",
+    "Встретился, раз",
+    "Оценка",
+    "Избранное",
+    "-",
+  ];
 
-  const columnNames = ['Имя', 'Качества', 'Профессия', 'Встретился, раз', 'Оценка', 'Избранное','-']
-
-  const count = users.length
-  const pageSize = 4
+  const count = users.length;
+  const pageSize = 4;
 
   const handlePageChange = (pageIndex) => {
-    console.log('page', pageIndex);
-  }
+    console.log("page", pageIndex);
+  };
 
   return (
     <>
-      {count > 0 &&
-        <table className='table'>
+      {count > 0 && (
+        <table className="table">
           <thead>
             <tr>
-              {columnNames.map(name => <th scope='col' key={name}>{name}</th>)}
+              {columnNames.map((name) => (
+                <th scope="col" key={name}>
+                  {name}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {users.map(user => <User key={user._id} {...user} {...rest}/>)}
+            {users.map((user) => (
+              <User key={user._id} {...user} {...rest} />
+            ))}
           </tbody>
         </table>
-      }
-      <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange}/>
+      )}
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      />
     </>
-  )
-
-}
+  );
+};
 
 export default Users
