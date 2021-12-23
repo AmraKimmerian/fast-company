bc15d47efad1b4c9123f47b4368a31424bce70d8
Anton <amrakimmerian@gmail.com>
2021-12-23 12:19:54 +0500

    React Route. Задание по урокам.

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,20 @@
+import NavBar from './component/navBar'
+import { Route, Switch } from 'react-router-dom'
+import Main from './component/main'
+import Login from './component/login'
 import Users from './component/users'
 
 const App = () => {
-  return <Users />
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/users/:userId?" component={Users} />
+        <Route path="/" component={Main} />
+      </Switch>
+    </>
+  )
 }
 
 export default App
---
+++ src/app/component/user.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import api from '../api'
+import QualitiesList from './qualitiesList'
+
+const User = ({ userId }) => {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    api.users.getById(userId).then((result) => {
+      console.log(result)
+      setUser(result)
+    })
+  }, [])
+
+  const history = useHistory()
+  const handleReturnToUsers = () => {
+    history.push('/users')
+  }
+
+  return user ? (
+    <div className="card m-1" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{user.name}</h5>
+        <h6 className="card-subtitle">{`Профессия: ${user.profession.name}`}</h6>
+        <QualitiesList qualities={user.qualities} />
+        <p className="xcard-tet">{`Встреч: ${user.completedMeetings}`}</p>
+        <h6 className="card-subtitle">{`Рейтинг: ${user.rate}`}</h6>
+        <button className="btn btn-primary mt-2" onClick={handleReturnToUsers}>
+          Все пользователи
+        </button>
+      </div>
+    </div>
+  ) : (
+    <h1>Loading</h1>
+  )
+}
+
+export default User
