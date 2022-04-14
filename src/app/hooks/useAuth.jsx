f741130605450dcba422ac0e71ff2790bb0685cf
Anton <amrakimmerian@gmail.com>
2022-04-14 13:49:16 +0500

    Добавление rate и completeMeetings

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -20,6 +20,9 @@ const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({})
   const [error, setError] = useState(null)
 
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
   async function signUp({ email, password, ...rest }) {
     try {
       const { data } = await httpAuth.post(`accounts:signUp`, {
@@ -28,7 +31,13 @@ const AuthProvider = ({ children }) => {
         returnSecureToken: true
       })
       setTokens(data)
-      await createUser({ _id: data.localId, email, ...rest })
+      await createUser({
+        _id: data.localId,
+        email,
+        rate: randomInt(1, 5),
+        completedMeetings: randomInt(0, 200),
+        ...rest
+      })
     } catch (error) {
       errorCatcher(error)
       const { code, message } = error.response.data.error
