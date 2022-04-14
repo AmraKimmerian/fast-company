f1b01f84137b2921e862aba3e0401bb08d1cf783
Anton <amrakimmerian@gmail.com>
2022-04-14 14:51:15 +0500

    Получение user при загрузке

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import axios from 'axios'
 import userService from '../services/user.service'
 import { toast } from 'react-toastify'
-import { setTokens } from '../services/localstorage.service'
+import localStorageService, {
+  setTokens
+} from '../services/localstorage.service'
 
 const httpAuth = axios.create({
   baseURL: 'https://identitytoolkit.googleapis.com/v1/',
@@ -61,6 +63,7 @@ const AuthProvider = ({ children }) => {
         returnSecureToken: true
       })
       setTokens(data)
+      getUserData()
     } catch (error) {
       errorCatcher(error)
       const { code, message } = error.response.data.error
@@ -78,7 +81,8 @@ const AuthProvider = ({ children }) => {
 
   async function createUser(data) {
     try {
-      const { content } = userService.create(data)
+      const { content } = await userService.create(data)
+      console.log(content)
       setCurrentUser(content)
     } catch (error) {
       errorCatcher(error)
@@ -89,6 +93,20 @@ const AuthProvider = ({ children }) => {
     const { message } = error.response.data
     setError(message)
   }
+
+  async function getUserData() {
+    try {
+      const { content } = await userService.getCurrentUser()
+      setCurrentUser(content)
+    } catch (error) {
+      errorCatcher(error)
+    }
+  }
+  useEffect(async () => {
+    if (localStorageService.getAccessToken()) {
+      getUserData()
+    }
+  }, [])
 
   useEffect(() => {
     if (error !== null) {
--- src/app/services/localstorage.service.js
+++ src/app/services/localstorage.service.js
@@ -1,29 +1,39 @@
 const TOKEN_KEY = 'jwt-token'
 const REFRESH_KEY = 'jwt-refresh-token'
 const EXPIRES_KEY = 'jwt-expires'
+const USERID_KEY = 'user-local-id'
 
-export function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
+export function setTokens({
+  idToken,
+  refreshToken,
+  localId,
+  expiresIn = 3600
+}) {
   const expiresDate = new Date().getTime() + expiresIn * 1000
   localStorage.setItem(TOKEN_KEY, idToken)
   localStorage.setItem(REFRESH_KEY, refreshToken)
+  localStorage.setItem(USERID_KEY, localId)
   localStorage.setItem(EXPIRES_KEY, expiresDate)
 }
 export function getAccessToken() {
-  localStorage.getItem(TOKEN_KEY)
+  return localStorage.getItem(TOKEN_KEY)
 }
 
 export function getRefreshToken() {
-  localStorage.getItem(REFRESH_KEY)
+  return localStorage.getItem(REFRESH_KEY)
 }
 
 export function getTokenExpiresDate() {
-  localStorage.getItem(EXPIRES_KEY)
+  return localStorage.getItem(EXPIRES_KEY)
 }
-
+export function getUserId() {
+  return localStorage.getItem(USERID_KEY)
+}
 const localStorageService = {
   setTokens,
   getAccessToken,
   getRefreshToken,
-  getTokenExpiresDate
+  getTokenExpiresDate,
+  getUserId
 }
 export default localStorageService
