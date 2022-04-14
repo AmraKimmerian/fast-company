ffecb85191c10a2f5ca18eeb5ccfbe62b6295f29
Anton <amrakimmerian@gmail.com>
2022-04-14 17:23:41 +0500

    Доработка NavBar

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,24 +1,38 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
+import NavProfile from './navProfile'
 
 const NavBar = () => {
+  const { currentUser } = useAuth()
+  console.log(currentUser)
   return (
-    <ul className="nav">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">
-          Main
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/login">
-          Login
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/users">
-          Users
-        </Link>
-      </li>
-    </ul>
+    <nav className="navbar bg-light mb-3">
+      <div className="container-fluid">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Main
+            </Link>
+          </li>
+          {currentUser && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Users
+              </Link>
+            </li>
+          )}
+        </ul>
+        <div className="d-flex">
+          {currentUser ? (
+            <NavProfile />
+          ) : (
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
   )
 }
 export default NavBar
