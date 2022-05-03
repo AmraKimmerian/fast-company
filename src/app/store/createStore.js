ce24096344f5b24afd4932f94289e78b7fac8256
Anton <amrakimmerian@gmail.com>
2022-05-03 10:54:33 +0500

    Redux. Создаем Store

---
+++ src/app/store/createStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+const rootReducer = combineReducers({})
+
+export function createStore() {
+  return configureStore({ reducer: rootReducer })
+}
